c9480b9bac438404d360cd35548054196e578711
JeongbinYoon <yon980824@gmail.com>
2024-03-18 22:53:42 +0900

    음성인식 텍스트 변환

---
+++ src/components/speech.tsx
@@ -0,0 +1,41 @@
+'use client';
+import 'regenerator-runtime/runtime';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
+
+export default function Speech() {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  //   if (!browserSupportsSpeechRecognition) {
+  //     return (
+  //         <span>{`Browser doesn't support speech recognition.`}</span>
+  //     );
+  //   }
+  console.log('transcript', transcript);
+
+  const startListening = () =>
+    SpeechRecognition.startListening({
+      continuous: false,
+      language: 'en',
+    });
+
+  const stopListening = () => {
+    SpeechRecognition.stopListening();
+  };
+  return (
+    <div>
+      <p>asd</p>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={startListening}>Start</button>
+      <button onClick={stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+}
