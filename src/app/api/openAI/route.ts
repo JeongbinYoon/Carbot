cd1a0b6ac31e755029545afdb0e5ba47795343a3
JeongbinYoon <yon980824@gmail.com>
2024-03-22 22:15:25 +0900

    OpenAI 연동

---
+++ src/app/api/openAI/route.ts
@@ -0,0 +1,67 @@
+import * as dotenv from 'dotenv';
+import Configuration from 'openai';
+import { OpenAI } from 'openai';
+import { NextResponse } from 'next/server';
+
+const config = {
+  apiKey: process.env.OPENAI_API_KEY,
+};
+const openai = new OpenAI(config);
+
+export async function POST(req: Request) {
+  if (!config.apiKey) {
+    NextResponse.json(
+      {
+        error: {
+          message: 'OpenAI API key not configured',
+        },
+      },
+      { status: 500 }
+    );
+    return;
+  }
+  const data = await req.json();
+  const question = data.question || '';
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You will be provided with a block of text, and your task is to extract a list of keywords from it.',
+      },
+      {
+        role: 'user',
+        content: question,
+      },
+    ],
+    temperature: 0.5,
+    max_tokens: 64,
+    top_p: 1,
+  });
+  console.log(response);
+  return NextResponse.json(response.choices[0].message);
+}
+
+// import OpenAI from 'openai'
+// import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// })
+
+// export const runtime = 'edge'
+
+// export async function POST(req: Request) {
+//   const { messages } = await req.json()
+//   const response = await openai.chat.completions.create({
+//     model: 'gpt-3.5-turbo',
+//     stream: true,
+//     messages,
+//   })
+
+//   const stream = OpenAIStream(response)
+
+//   return new StreamingTextResponse(stream)
+// }
--- src/components/speech.tsx
+++ src/components/speech.tsx
@@ -33,14 +33,35 @@ export default function Speech() {
   const stopListening = () => {
     SpeechRecognition.stopListening();
   };
+
+  // openAI 연동
+  const handleSubmit = async () => {
+    const question =
+      "Black-on-black ware is a 20th- and 21st-century pottery tradition developed by the Puebloan Native American ceramic artists in Northern New Mexico. Traditional reduction-fired blackware has been made for centuries by pueblo artists. Black-on-black ware of the past century is produced with a smooth surface, with the designs applied through selective burnishing or the application of refractory slip. Another style involves carving or incising designs and selectively polishing the raised areas. For generations several families from Kha'po Owingeh and P'ohwhóge Owingeh pueblos have been making black-on-black ware with the techniques passed down from matriarch potters. Artists from other pueblos have also produced black-on-black ware. Several contemporary artists have created works honoring the pottery of their ancestors.";
+
+    try {
+      const res = await fetch('http://localhost:3000/api/openAI', {
+        method: 'POST',
+        body: JSON.stringify({ question }),
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
+
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
-      <p>asd</p>
       <p>Microphone: {listening ? 'on' : 'off'}</p>
       <button onClick={startListening}>Start</button>
       <button onClick={stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
       <p>{transcript}</p>
+
+      <button onClick={handleSubmit}>TEST</button>
     </div>
   );
 }
